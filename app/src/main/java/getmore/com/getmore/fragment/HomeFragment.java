3e6fdd0dde5ad0055055bfb63c7c8de7792b57fc
ngzhongqin <ngzhongqin@gmail.com>
2014-12-31 02:56:32 +0800

    Loaded Icons for Home, Friends and Points on Top Menu Button Listener in ActionBarHandler Buttons are now selectable and colours changes accordingly. Implemented first webservice call to server side Implemented small spinner for ID on landing page. Removed unwanted fragments Built Skeleton of ViewPager. Added Search Btn Added line under action bar Added yellow box under selected menu option Added Search Hints Added Search Box Added Set visibility for search box and hint

--- app/src/main/java/getmore/com/getmore/fragment/HomeFragment.java
+++ app/src/main/java/getmore/com/getmore/fragment/HomeFragment.java
@@ -5,11 +5,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import getmore.com.getmore.R;
 
 public class HomeFragment extends Fragment {
     public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
+    private ViewHolder viewHolder;
+    private View v;
+
+    class ViewHolder{
+        FrameLayout search_box_container;
+        LinearLayout search_hint_container;
+    }
 
     public static final HomeFragment newInstance(String message)
     {
@@ -21,12 +30,31 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         String message = getArguments().getString(EXTRA_MESSAGE);
-        View v = inflater.inflate(R.layout.home_f, container, false);
+        v = inflater.inflate(R.layout.home_f, container, false);
+        viewHolder=new ViewHolder();
+        viewHolder.search_box_container = (FrameLayout) v.findViewById(R.id.search_box_container);
+        viewHolder.search_hint_container = (LinearLayout) v.findViewById(R.id.search_hint_container);
+        setSearchBoxContainerVisible(false);
 
         return v;
+    }
+
+    public void setSearchBoxContainerVisible(boolean visible){
+        if(visible){
+            viewHolder.search_box_container.setVisibility(View.VISIBLE);
+        }else{
+            viewHolder.search_box_container.setVisibility(View.GONE);
+        }
+    }
+
+    public void setSearchHintContainerVisible(boolean visible){
+        if(visible){
+            viewHolder.search_hint_container.setVisibility(View.VISIBLE);
+        }else{
+            viewHolder.search_hint_container.setVisibility(View.GONE);
+        }
     }
 }
 
